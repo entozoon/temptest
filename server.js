5dff4fd1caa90395f98d105cd377c407e13ef12b
Michael Cook <michael.cook@techdept.co.uk>
2017-07-11 16:42:52 +0100

    before i bleh

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
 // Dependencies
-const server = require('http').createServer(),
-  io = require('socket.io')(server),
+const app = require('express')(),
   path = require('path'),
   open = require('open');
 
 // Variables
 const port = 3000;
 
-// Create a simple http server. No need to use express for such a simple SPA.
-server
-  .createServer((req, res) => {
-    var uri = url.parse(req.url).pathname;
-    var filename = path.join(process.cwd(), uri);
-    fs.exists(filename, exists => {
-      if (!exists) {
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.write('404 Not Found\n');
-        res.end();
-        return;
-      }
-      var mimeType = mimeTypes[path.extname(filename).split('.')[1]];
-      res.writeHead(200, { 'Content-Type': mimeType });
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
-      var fileStream = fs.createReadStream(filename);
-      fileStream.pipe(res);
-    });
-  })
-  .listen(port, () => {
+// Create a simple http server using express
+const server = app.listen(port, function(error) {
+  if (error) {
+    console.log(error);
+  } else {
     let url = 'http://localhost:' + port;
     console.log('Listening at ' + url);
 
-    // Open URL automatically, for convenience
+    // Open URL automagically, for convenience
     open(url);
-  });
+  }
+});
+
+const io = require('socket.io')(server);
 
 io.on('connection', function(client) {
   client.on('event', function(data) {
